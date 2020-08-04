c4666b560036170723efa1546180a25e551cd2d5
Андрей Сошин <a_soshin@mail.ru>
2020-08-04 23:32:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2) throw new Exception();
+                string inputFile = args[0];
+                string outputFile = args[1];
+                if (!File.Exists(args[0])) throw new Exception();
+                var lines = File.ReadLines(inputFile).OrderBy(x => x).ToArray();
+                File.WriteAllLines(outputFile, lines);
+
+                Environment.ExitCode = 0;
+                Console.WriteLine("ExiteCode = 0");
+            }
+            catch
+            {
+                Environment.ExitCode = 1;
+                Console.WriteLine("ExiteCode = 1");
+            }
+        }
+    }
+}
